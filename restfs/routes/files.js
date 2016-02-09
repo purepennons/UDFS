160d1a3086e89994376702f31a686e365353be11
Chiahao Lin <y80811@gmail.com>
2016-02-09 17:35:38 +0800

    Return file list * If with list_only field, it will only return the list of filenames. * Otherwise, it will return the metadata of files.

--- restfs/routes/files.js
+++ restfs/routes/files.js
@@ -1,23 +1,29 @@
 "use strict"
 
+import path from 'path'
 import express from 'express'
-import bluebird from 'bluebird'
+import Promise from 'bluebird'
 
 const router = express.Router()
-const fs = bluebird.promisifyAll(require('fs'))
+const fs = Promise.promisifyAll(require('fs'))
 
 router.get('/', (req, res, next) => {
-  // field: ['metadata', 'stat', 'type', 'path', 'mount_path', 'content', 'list']
+  // field: ['metadata', 'stat', 'type', 'path', 'mount_path', 'content', 'filename', 'size', 'list_only']
   let field = req.query.field
-
   // no field => always return stat, path, mount_path and type
   if(!field) {
     return res.status(200).json({
-      name: req.filename,
-      path: req.file_path,
-      mount_path: req.file_path,
-      type: req.type,
-      stat: req.stat
+      status: 'success',
+      message: '',
+      data: [
+        {
+          name: req.filename,
+          path: req.file_path,
+          mount_path: req.file_path,
+          type: req.type,
+          stat: req.stat
+        }
+      ]
     })
   }
 
@@ -26,8 +32,48 @@ router.get('/', (req, res, next) => {
   } else {
     // return the list of the directory
     async function readdir() {
-      let files = await fs.readdirAsync(req.file_path)
-      console.log(files)
+      try {
+        let files = await fs.readdirAsync(req.file_path)
+
+        // field with 'list_only' will only return the array of filenames
+        if(field.indexOf('list_only') > -1) {
+          console.log(field.indexOf('list_only') );
+          return res.status(200).json({
+            status: 'success',
+            message: '',
+            data: files
+          })
+        }
+
+        // return metadata for each file
+        let results = await Promise.map(files, (file, index, length) => {
+          return new Promise( (resolve, reject) => {
+            fs.statAsync( path.join(req.file_path, file) )
+            .then( st => {
+              return resolve({
+                filename: file,
+                type: (st.isDirectory())? 'directory': 'file',
+                path: req.file_path,
+                mount_path: req.file_path,
+                size: st.size,
+                stat: st
+              })
+            })
+            .catch( err => {
+              return reject(err)
+            })
+          })
+        })
+
+        return res.status(200).json({
+          status: 'success',
+          message: '',
+          data: results
+        })
+
+      } catch( catchError ) {
+        throw catchError
+      }
     }
 
     readdir().then().catch( catchError => {
@@ -36,9 +82,6 @@ router.get('/', (req, res, next) => {
     })
   }
 
-
-  res.send('hello world')
-  return res.end
 })
 
 export default router
