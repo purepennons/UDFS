d1b7d36109e3ba3df9682f9b9b3c010f1c3204bc
Chiahao Lin <y80811@gmail.com>
2016-04-06 02:44:12 +0800

    refactor the init.js to elegant style and add process status handler

--- src/init.js
+++ src/init.js
@@ -4,48 +4,67 @@ const config = require('./config/config.json')
 const SecureDB = require('./secure_db/index')
 const UFS = require('./fuse/index')
 
+let db = undefined
+let ufs = undefined
+
 // initial
-try {
+async function init() {
+  try {
+    // db setting
+    let db_location = config.db.location || './data/db'
+    let db_options = config.db.options
 
-  // db setting
-  let db_location = config.db.location || './data/db'
-  let db_options = config.db.options
+    // connect to db
+    // db.db is the instance of leveldb
+    db = new SecureDB(db_location, db_options)
 
-  // connect to db
-  // db.db is the instance of leveldb
-  let db = new SecureDB(db_location, db_options)
+    // initial the file system
+    let fs_options = config.fuse
+    ufs = new UFS(fs_options.mnt, db, {
+      fuse_ops: {
+        options: fs_options.options
+      }
+    })
 
-  // initial the file system
-  let fs_options = config.fuse
-  let ufs = new UFS(fs_options.mnt, db, {
-    fuse_ops: {
-      options: fs_options.options
-    }
-  })
+    // mount fs
+    await ufs.mount()
+    console.log(`File system is mounted at ${fs_options.mnt}`)
 
-  // just for cleanup
-  global.ufs = ufs
+  } catch(err) {
+    console.error('Initialization failed', err.stack)
+    throw err
+  }
 
-  ufs.mount().then( () => {
-    console.log('mount success')
-  }).catch( err => {
-    console.error('Mount the file system failed.', err.stack)
-  })
-
-
-} catch(err) {
-  console.error('Initialization failed', err.stack)
-  process.exit(1)
+  return
 }
 
-process.on('exit', () => {
-  // cleanup
-  global.ufs.unmount().then(()=> console.log('System exit.'))
-})
+// exit handler
+function cleanup() {
+  if(ufs) ufs.unmount()
+  return
+}
 
-process.on('SIGINT', () => {
-  global.ufs.unmount().then( () => {
-    process.exit(0)
-    console.log('System exit.')
-  })
-})
+function exitHandler(options={}, err) {
+
+  async function exitGen() {
+    if(err) console.error('uncaughtException', err.stack)
+    if(options.clean) await cleanup()
+    if(options.exit) process.exit()
+    return
+  }
+
+  exitGen().catch(err => console.error(err))
+}
+
+// catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {clean: true, exit: true}))
+
+// catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {clean: true, exit: true}))
+
+// process close
+process.on('exit', exitHandler.bind(null, {clean: true}))
+
+
+// boot
+init().catch(err => console.error(err))
