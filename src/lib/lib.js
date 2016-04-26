49029bece594f825e70848cb2d865aa333b381d5
Chiahao Lin <y80811@gmail.com>
2016-04-26 17:27:37 +0800

    lib: statWrapper, call this before inserting it will update the info of stat

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const _ = require('lodash')
+const uuid = require('node-uuid')
 
 exports.parseFlag = function(flag) {
   switch(flag) {
@@ -11,6 +12,28 @@ exports.parseFlag = function(flag) {
     case 2:
       return 'r+'
   }
+}
+
+// pre-process the stat of file before inserting
+// especially generates the file_id, uid, gid if they do not exist.
+// Also updates the atime
+// It will set the status to false if without the status of options
+exports.statWrapper = function(s, type, status=false, options={atimeUpdate: true}) {
+  if(!s) throw new Error('without the stat param')
+
+  // options pre-process
+  if(!(_.has(options, 'atimeUpdate'))) options.atimeUpdate = true
+
+  // must check
+  s.file_id = s.file_id || uuid.v1() // timestamp-based uuid
+  s.uid = s.uid || process.getuid()
+  s.gid = s.gid || process.getgid()
+
+  // update
+  s.status = status
+  if(options.atimeUpdate) s.atime = new Date()
+
+  return s
 }
 
 function genUniqueKeyFromMap(m, start=0, range=65535) {
