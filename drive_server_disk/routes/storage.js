3f4b6ab1f4a1774164c7254acd57e619146eec23
Chiahao Lin <y80811@gmail.com>
2016-05-13 23:34:54 +0800

    disk_drive: getFS is work

--- drive_server_disk/routes/storage.js
+++ drive_server_disk/routes/storage.js
@@ -4,23 +4,30 @@ const router = require('express').Router()
 const lib = require('../lib/lib')
 
 // modules of api routers
-const v1_files = require('../drive/files/v1/index')
 const v1_fs = require('../drive/fs/v1/index')
+const v1_files_meta = require('../drive/files_meta/v1/index')
+const v1_files = require('../drive/files/v1/index')
 
 // version v1
 const v1 = lib.versionWrapper('v1')
 
-// file operations
-router.get(v1('/:fs_id/:object_id'), v1_files.getFile)
-router.post(v1('/:fs_id/:object_id'), v1_files.createFile)
-router.put(v1('/:fs_id/:object_id'), v1_files.updateFile)
-router.delete(v1('/:fs_id/:object_id'), v1_files.deleteFile)
-router.head(v1('/:fs_id/:object_id'), v1_files.getHead)
-
 // file system operations
 router.get(v1('/:fs_id'), v1_fs.getFS)
-router.post(v1('/:fs_id'), v1_fs.createFS)
+router.post(v1('/create'), v1_fs.createFS)
 router.put(v1('/:fs_id'), v1_fs.updateFS)
 router.delete(v1('/:fs_id'), v1_fs.deleteFS)
+
+// metadata of file
+router.get(v1('/:fs_id/meta/:meta_id'), v1_files_meta.getMeta)
+router.post(v1('/:fs_id/meta/create'), v1_files_meta.createMeta)
+router.put(v1('/:fs_id/meta/:meta_id'), v1_files_meta.updateMeta)
+router.delete(v1('/:fs_id/meta/:meta_id'), v1_files_meta.deleteMeta)
+
+// file operations
+router.get(v1('/:fs_id/:object_id'), v1_files.getFile)
+router.post(v1('/:fs_id/create'), v1_files.createFile)
+router.put(v1('/:fs_id/:object_id'), v1_files.updateFile)
+router.delete(v1('/:fs_id/:object_id'), v1_files.deleteFile)
+
 
 module.exports = router
