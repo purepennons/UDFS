d1feaf41ddb2c00a96697962526cdb9677e5b87b
Chiahao Lin <y80811@gmail.com>
2016-08-28 23:55:08 +0800

    drive_server_disk#updateFile: refactor and add error handling for uploading

--- drive_server_disk/drive/files/v1/updateFile.js
+++ drive_server_disk/drive/files/v1/updateFile.js
@@ -37,107 +37,114 @@ module.exports = function updateFile(req, res, next) {
   fs.accessAsync(target, fs.F_OK)
   .then(() => {
     // read old meta
-    return fs.readFileAsync(meta_target)
+    return fs.readFileAsync(meta_target, {encoding: 'utf8'})
   })
   .then(old_meta => {
-    old_meta = JSON.parse(old_meta) // assume that it will never be failed.
+    try {
+      debug('old_meta', old_meta)
+      old_meta = JSON.parse(old_meta) // assume that it will never be failed.
 
-    // check the size of file is matched to the range before updating
-    if(range[0].start > old_meta.stat.size) {
-      let err = new Error('out of range')
-      err.status = 416
-      return next(err)
-    }
+      // check the size of file is matched to the range before updating
+      if(range[0].start > old_meta.stat.size) {
+        let err = new Error('out of range')
+        err.status = 416
+        return next(err)
+      }
 
-    let form = new multiparty.Form()
-    let uploadFlag = false  // only allow one file to be uploaded
+      let form = new multiparty.Form()
+      let uploadFlag = false  // only allow one file to be uploaded
 
-    form.on('error', err => {
-      debug('err', err)
-      err.status = 500
-      next(err)
-    })
-    .on('part', part => {
-      // part is a readable stream
-      if(!part.filename || uploadFlag) {
-        return part.resume()
-      } else {
-        // truncate the file to the correct size before pipeing the stream
-        fs.truncate(target, range[0].start, err => {
-          debug(`truncate the size of ${object_id} file to ${range[0].start}`)
-          if(err) {
-            debug('err', err)
-            err.status = 500
-            err.message = 'something wrong when truncate the file'
-            return next(err)
-          }
-
-          uploadFlag = true
-          let ws = fs.createWriteStream(target, {
-            flags: 'r+',
-            start: range[0].start
-          })
-          .on('error', err => {
-            debug('err', err)
-            err.status = 500
-            err.message = 'something wrong when writing the data'
-            return next(err)
-          })
-          .on('close', () => {
-            async function updateMeta() {
-              let stat = await fs.statAsync(target)
-              // debug('stat', stat)
-
-              // update the metadata
-              let meta = {}
-              meta['stat'] = xtend(old_meta['stat'], stat)
-              let meta_fd = await fs.openAsync(meta_target, 'w')
-              await fs.writeAsync(meta_fd, JSON.stringify(meta))
-
-              // release
-              fs.close(meta_fd)
-              return meta
-            }
-
-            // update and response
-            updateMeta()
-            .catch(err => {
+      form.on('error', err => {
+        debug('err', err)
+        err.status = 500
+        next(err)
+      })
+      .on('part', part => {
+        // part is a readable stream
+        if(!part.filename || uploadFlag) {
+          return part.resume()
+        } else {
+          // truncate the file to the correct size before pipeing the stream
+          fs.truncate(target, range[0].start, err => {
+            debug(`truncate the size of ${object_id} file to ${range[0].start}`)
+            if(err) {
               debug('err', err)
               err.status = 500
-              err.message = 'something wrong when updating the metadata'
-              return next(err)
-            })
-          })
-
-          // listen the status of stream
-          eos(part, err => {
-            if(err) {
-              debug('err', err.stack)
-              err.status = 500
-              err.message = 'upload stream failed'
+              err.message = 'something wrong when truncate the file'
               return next(err)
             }
 
-            debug('part stream end')
+            uploadFlag = true
+            let ws = fs.createWriteStream(target, {
+              flags: 'r+',
+              start: range[0].start
+            })
+            .on('error', err => {
+              debug('err', err)
+              err.status = 500
+              err.message = 'something wrong when writing the data'
+              return next(err)
+            })
+            .on('close', () => {
+              async function updateMeta() {
+                let stat = await fs.statAsync(target)
+                // debug('stat', stat)
+
+                // update the metadata
+                let meta = {}
+                meta['stat'] = xtend(old_meta['stat'], stat)
+                let meta_fd = await fs.openAsync(meta_target, 'w')
+                await fs.writeAsync(meta_fd, JSON.stringify(meta))
+
+                // release
+                fs.close(meta_fd)
+                return meta
+              }
+
+              // update and response
+              updateMeta()
+              .catch(err => {
+                debug('err', err)
+                err.status = 500
+                err.message = 'something wrong when updating the metadata'
+                return next(err)
+              })
+            })
+
+            // listen the status of stream
+            eos(part, err => {
+              if(err) {
+                debug('err', err.stack)
+                err.status = 500
+                err.message = 'upload stream failed'
+                return next(err)
+              }
+
+              debug('part stream end')
+            })
+
+            // write the data
+            part.pipe(ws)
+
           })
-
-          // write the data
-          part.pipe(ws)
-
-        })
-      }
-    })
-    .on('close', () => {
-      debug('form end')
-      return res.status(200).json({
-        status: 'success',
-        message: `${object_id} object was updated from ${range[0].start} position`,
-        data: []
+        }
       })
-    })
+      .on('close', () => {
+        debug('form end')
+        return res.status(200).json({
+          status: 'success',
+          message: `${object_id} object was updated from ${range[0].start} position`,
+          data: []
+        })
+      })
 
-    // req parse
-    form.parse(req)
+      // req parse
+      form.parse(req)
+    } catch(err) {
+      err.status = 500
+      err.message = 'the format of metadata is not a json'
+      return next(err)
+    }
   })
   .catch(err => {
     if(err.code === 'ENOENT') return next()
