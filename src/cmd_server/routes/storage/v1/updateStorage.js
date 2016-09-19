a67abf8f11346177388661857f965865b5a9778d
Chiahao Lin <y80811@gmail.com>
2016-09-20 02:18:53 +0800

    cmd-server#updateStorage: add update storage info router

---
+++ src/cmd_server/routes/storage/v1/updateStorage.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const debug = require('debug')('cmd-server#updateStorage')
+
+module.exports = function updateStorage(req, res, next) {
+
+  const fuseContext = req.app.get('fuseContext')
+  const db = fuseContext.db
+  const io = fuseContext.io
+
+  let storage_id = req.params.storage_id
+  let url = req.body.url || undefined // ignore now
+  let auth = req.body.auth || undefined // ignore now
+  let otherInfo = req.body.otherInfo || undefined
+
+  const update = async () => {
+    try {
+      return await io.updateStorage(storage_id, url, auth, otherInfo)
+    } catch(err) {
+      err.status = 500
+      throw err
+    }
+  }
+
+  update()
+  .then(storage_info => {
+    debug('storage_info', storage_info)
+    return res.status(200).json({
+      status: 'success',
+      message: `information of storage-${storage_id} has been updated.`,
+      data: [storage_info]
+    })
+  })
+  .catch(err => next(err))
+}
