d6bd2deecb1674f375e94e123f6c5ddd6a6cff35
Chiahao Lin <y80811@gmail.com>
2016-10-26 00:35:35 +0800

    add exp2_io_bound_single_file_blk_64k.sh script

---
+++ experiment/exp2_io_bound_single_file_blk_64k.sh
@@ -0,0 +1,29 @@
+# write
+shjs write_single.js 64k 16000 10 1gb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_1gb_single
+shjs write_single.js 64k 8000 10 500mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_500mb_single
+shjs write_single.js 64k 1600 10 100mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_100mb_single
+shjs write_single.js 64k 160 10 10mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_10mb_single
+shjs write_single.js 64k 16 10 1mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_1mb_single
+shjs write_single.js 64k 1 10 64k_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_64k_single
+
+shjs write_single.js 64k 16000 10 1gb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_1gb_single
+shjs write_single.js 64k 8000 10 500mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_500mb_single
+shjs write_single.js 64k 1600 10 100mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_100mb_single
+shjs write_single.js 64k 160 10 10mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_10mb_single
+shjs write_single.js 64k 16 10 1mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_1mb_single
+shjs write_single.js 64k 1 10 64k_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_64k_single
+
+# read
+shjs read_single.js 64k 16000 10 1gb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_1gb_single
+shjs read_single.js 64k 8000 10 500mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_500mb_single
+shjs read_single.js 64k 1600 10 100mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_100mb_single
+shjs read_single.js 64k 160 10 10mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_10mb_single
+shjs read_single.js 64k 16 10 1mb_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_1mb_single
+shjs read_single.js 64k 1 10 64k_single /home/root/thesis_src/thesis/src/mnt/storage_hdd_e2/exp_64k_single
+
+shjs read_single.js 64k 16000 10 1gb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_1gb_single
+shjs read_single.js 64k 8000 10 500mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_500mb_single
+shjs read_single.js 64k 1600 10 100mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_100mb_single
+shjs read_single.js 64k 160 10 10mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_10mb_single
+shjs read_single.js 64k 16 10 1mb_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_1mb_single
+shjs read_single.js 64k 1 10 64k_single /home/root/thesis_src/thesis/src/mnt/storage_ssd_e2/exp_64k_single
